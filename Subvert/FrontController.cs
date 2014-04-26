dc5ba6cbdf73008e789f76e476e1b4ef17f5b042
andy <andy@andydote.co.uk>
2014-04-26 15:43:58 +0100

    frontController: hard code some routing and endpoint handling to prove it works

--- Subvert/FrontController.cs
+++ Subvert/FrontController.cs
@@ -1,19 +1,45 @@
-﻿using System.Net;
+﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Reflection;
+using System.Text;
 using System.Web.Http;
+using StructureMap;
 
 namespace Subvert
 {
 	internal class FrontController : ApiController
 	{
-		public FrontController()
-		{
+		private readonly Router _router;
+		private readonly IJsonSerializer _serializer;
+		private readonly Container _container;
 
+		public FrontController(Router router, IJsonSerializer serializer)
+		{
+			_router = router;
+			_serializer = serializer;
 		}
 
 		public HttpResponseMessage Handle()
 		{
-			return new HttpResponseMessage(HttpStatusCode.OK);
+			var endpoint = _router.GetEndpointForRoute(Request.RequestUri.PathAndQuery);
+			var method = endpoint.GetMethod("Get");
+			var modelType = method.GetParameters().First().ParameterType;
+
+			var modelInstance = modelType.GetConstructor(Type.EmptyTypes).Invoke(new object[] { });
+
+			var instance = endpoint.GetConstructor(Type.EmptyTypes).Invoke(new object[] { });
+
+			var viewModel = method.Invoke(instance, new[] { modelInstance });
+
+			//content negotiation to work out how to send it back...json for now
+
+			var json = _serializer.Serialize(viewModel);
+			return new HttpResponseMessage(HttpStatusCode.OK)
+			{
+				Content = new StringContent(json, Encoding.UTF8, "text/json")
+			};
 		}
 	}
 }
---
+++ Subvert/Router.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Subvert
+{
+	public class Router
+	{
+		private readonly EndpointDiscovery _endpoints;
+
+		public Router(EndpointDiscovery endpoints)
+		{
+			_endpoints = endpoints;
+		}
+
+		public Type GetEndpointForRoute(string route)
+		{
+			return _endpoints.GetEndpoints().First();
+		}
+	}
+}
