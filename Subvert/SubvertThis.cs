f3b602fda90eeb592eef106f8be186ebb767424e
andy <andy@andydote.co.uk>
2014-04-26 15:31:56 +0100

    subvert: add structuremap dependency

--- Subvert/SubvertThis.cs
+++ Subvert/SubvertThis.cs
@@ -1,15 +1,37 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
+using StructureMap;
+using StructureMap.Graph;
+using Subvert.Infrastructure;
 
 namespace Subvert
 {
 	public static class SubvertThis
 	{
-		public static void Configuration(HttpConfiguration config)
+		public static void Configuration(Type hostType, HttpConfiguration config)
 		{
+			var container = new Container(c =>
+			{
+				c.Scan(a =>
+				{
+					a.TheCallingAssembly();
+					a.LookForRegistries();
+					a.WithDefaultConventions();
+				});
+
+				c.For<HostAssembly>()
+					.Use(() => new HostAssembly(hostType))
+					.Singleton();
+			});
+
+			config.DependencyResolver = new StructureMapDependencyResolver(container);
+
 			config.Services.Replace(typeof(IHttpActionSelector), new EndpointActionSelector());
 			config.Services.Replace(typeof(IHttpControllerSelector), new EndpointSelector<FrontController>(config));
+
+
 		}
 	}
 }
