c6fe949479bd910215d70b69d9a1ad7089e6a930
andy <andy@andydote.co.uk>
2014-04-26 15:41:03 +0100

    subvertThis: use a HostAssembly to mark the assembly we are subverting

---
+++ Subvert/HostAssembly.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace Subvert
+{
+	public class HostAssembly
+	{
+		private readonly Lazy<Assembly> _host;
+
+		public HostAssembly(Type typeInHost)
+		{
+			_host = new Lazy<Assembly>(() => typeInHost.Assembly);
+		}
+
+		public Assembly Assembly { get { return _host.Value; }}
+	}
+}
