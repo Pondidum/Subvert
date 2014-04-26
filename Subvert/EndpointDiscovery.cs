f9af12c6abd6a79f2e28b7393178acc8c834e02d
andy <andy@andydote.co.uk>
2014-04-26 15:42:05 +0100

    endpointDiscovery: find all endpoints based on the host assembly

---
+++ Subvert/EndpointDiscovery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Subvert
+{
+	public class EndpointDiscovery
+	{
+		private readonly Lazy<List<Type>> _types;
+
+		public EndpointDiscovery(HostAssembly hostAssembly)
+		{
+			_types = new Lazy<List<Type>>(() =>
+			{
+				return hostAssembly
+					.Assembly
+					.GetTypes()
+					.Where(t => t.Name.EndsWith("Endpoint", StringComparison.OrdinalIgnoreCase))
+					.ToList();
+			});
+		}
+
+		public IEnumerable<Type> GetEndpoints()
+		{
+			return _types.Value;
+		}
+	}
+}
