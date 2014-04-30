a00fccb02e34fc17e258cde75a79718f415d7718
andy <andy@andydote.co.uk>
2014-04-30 22:01:10 +0100

    endpointDiscovery: add .GetEndpointByName()

--- Subvert/EndpointDiscovery.cs
+++ Subvert/EndpointDiscovery.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Subvert
 {
@@ -24,6 +25,11 @@ namespace Subvert
 		public IEnumerable<Type> GetEndpoints()
 		{
 			return _types.Value;
+		}
+
+		public Type GetEndpointByName(string name)
+		{
+			return _types.Value.FirstOrDefault(e => e.Name.Equals(name + "Endpoint", StringComparison.OrdinalIgnoreCase));
 		}
 	}
 }
