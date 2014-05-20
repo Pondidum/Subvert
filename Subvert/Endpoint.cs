7b75f305714a10c270103716cdf7a946d34fc073
andy <andy@andydote.co.uk>
2014-05-20 23:17:26 +0100

    endpoint: handle multiple method names

--- Subvert/Endpoint.cs
+++ Subvert/Endpoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace Subvert
@@ -16,7 +17,23 @@ namespace Subvert
 
 		public EndpointAction GetAction(string httpMethod, string name)
 		{
-			var method = Type.GetMethod(httpMethod + name, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+			var methods = Type
+				.GetMethods(BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance)
+				.Where(m => m.Name.Equals(httpMethod + name, StringComparison.OrdinalIgnoreCase))
+				.Where(m => m.GetParameters().Count() == 1)
+				.ToList();
+
+			if (methods.Any() == false)
+			{
+				return null;
+			}
+
+			var method = methods.SingleOrDefault();
+
+			if (method == null)
+			{
+				return null;
+			}
 
 			return new EndpointAction(this, method);
 		}
