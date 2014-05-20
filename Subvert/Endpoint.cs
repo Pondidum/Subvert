b7dfe5dca395f3f194c5ec8c3d10247672095bda
andy <andy@andydote.co.uk>
2014-05-20 19:58:55 +0100

    endpoints: add custom classes to encapsulate endpoints and actions

---
+++ Subvert/Endpoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using StructureMap;
+
+namespace Subvert
+{
+	public class Endpoint
+	{
+		public string Name { get; private set; }
+		public Type Type { get; private set; }
+
+		public Endpoint(Type type, string endpointName)
+		{
+			Type = type;
+			Name = endpointName;
+		}
+
+		public EndpointAction GetAction(string httpMethod, string name)
+		{
+			var method = Type.GetMethod(httpMethod + name, BindingFlags.IgnoreCase);
+
+			return new EndpointAction(this, method);
+		}
+	}
+}
---
+++ Subvert/EndpointAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Subvert
+{
+	public class EndpointAction
+	{
+		private readonly MethodInfo _method;
+
+		public EndpointAction(Endpoint endpoint,MethodInfo method)
+		{
+			_method = method;
+
+			InputModelType = method
+				.GetParameters()
+				.Single()
+				.ParameterType;
+
+			EndpointType = endpoint.Type;
+		}
+
+		public Type InputModelType { get; private set; }
+		public Type EndpointType { get; private set; }
+
+		public object Run(object instance, object inputModel)
+		{
+			return _method.Invoke(instance, new[] { inputModel });
+		}
+	}
+}
