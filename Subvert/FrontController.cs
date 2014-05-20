5b796fba5f911bfcf11aa98465be0e0fc574644b
andy <andy@andydote.co.uk>
2014-05-20 20:30:55 +0100

    frontController: rework and hardcode route parsing

--- Subvert/FrontController.cs
+++ Subvert/FrontController.cs
@@ -22,21 +22,29 @@ namespace Subvert
 
 		public HttpResponseMessage Handle()
 		{
-			var endpoint = _router.GetEndpointForRoute(Request.RequestUri);
+			var route = new RouteData();
 
-			if (endpoint == null)
-			{
-				return new HttpResponseMessage(HttpStatusCode.NotFound);
-			}
-
-			var method = endpoint.GetMethod("Get");
-			var modelType = method.GetParameters().First().ParameterType;
-
+			var segments = Request.RequestUri.Segments.Where(s => s != "/").ToList();
+			route.Endpoint = segments.FirstOrDefault();
+			route.Action = segments.Skip(1).FirstOrDefault();
+			route.Method = Request.Method.Method;
+			
 			using (var container = _container.GetNestedContainer())
 			{
-				var inputModel = container.GetInstance(modelType);
-				var instance = container.GetInstance(endpoint);
-				var viewModel = method.Invoke(instance, new[] { inputModel });
+				var action = _router.GetAction(route);
+
+				if (action == null)
+				{
+					return new HttpResponseMessage(HttpStatusCode.NotFound);
+				}
+
+				var instance = container.GetInstance(action.EndpointType);
+				var inputModel = container.GetInstance(action.InputModelType);
+				
+
+				//populate inputModel
+
+				var viewModel = action.Run(instance, inputModel);
 
 				var renderer = _rendererFactory.ForContentType(Request.Headers.Accept);
 
