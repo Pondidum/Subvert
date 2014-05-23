3c23c5b32c317aa8ff481831e97361957a2e6a2f
andy <andy@andydote.co.uk>
2014-05-23 20:57:21 +0100

    modelBinding: add formData binding

---
+++ Subvert/ModelBinding/FormDataBinder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace Subvert.ModelBinding
+{
+	public class FormDataBinder : IModelBinder
+	{
+		private readonly DictionaryBinder _binder;
+
+		public FormDataBinder(DictionaryBinder binder)
+		{
+			_binder = binder;
+		}
+
+		public bool CanHandle(HttpRequestMessage message)
+		{
+			return message.Content.IsFormData();
+		}
+
+		public async void Bind(HttpRequestMessage message, object model)
+		{
+			var content = await message.Content.ReadAsFormDataAsync();
+
+			var collection = content
+				.Cast<string>()
+				.Select(key => new KeyValuePair<string, string>(key, content[key]));
+
+			_binder.Bind(collection, model);
+		}
+	}
+}
