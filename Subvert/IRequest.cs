f395ae2320395a293fe6ec0115bd45949d1c5fb8
andy <andy@andydote.co.uk>
2014-05-24 10:17:53 +0100

    request: start abstracting HttpRequestMessage

---
+++ Subvert/IRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Subvert
+{
+	public interface IRequest
+	{
+		IEnumerable<KeyValuePair<string, string>> Query { get; }
+		string HttpMethod { get; }
+
+		Uri RawUrl { get; }
+	}
+}
---
+++ Subvert/Request.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace Subvert
+{
+	public class Request : IRequest
+	{
+		public IEnumerable<KeyValuePair<string, string>> Query { get; private set; }
+		public string HttpMethod { get; private set; }
+		public Uri RawUrl { get; private set; }
+
+		public Request(HttpRequestMessage request)
+		{
+			HttpMethod = request.Method.Method;
+			Query = request.GetQueryNameValuePairs().ToList();
+			RawUrl = request.RequestUri;
+		}
+	}
+}
