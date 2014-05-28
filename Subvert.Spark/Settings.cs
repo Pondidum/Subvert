56ea587d675521f590d6efa3d08a8767e130c77b
andy <andy@andydote.co.uk>
2014-05-28 19:56:25 +0100

    sparkSettings: add a build for settings

---
+++ Subvert.Spark/Settings.cs
@@ -0,0 +1,24 @@
+﻿using Spark;
+
+namespace Subvert.Spark
+{
+	public class SettingsBuilder
+	{
+		public SparkSettings Settings { get; private set; }
+
+		public SettingsBuilder()
+		{
+			Settings = new SparkSettings
+			{
+				PageBaseType = typeof (SubvertSparkView).FullName
+			};
+
+			AddNamespace("System.Linq");
+		}
+
+		public void AddNamespace(string ns)
+		{
+			Settings.AddNamespace(ns);
+		}
+	}
+}
