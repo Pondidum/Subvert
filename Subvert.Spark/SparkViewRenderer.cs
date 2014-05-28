42b86beb1dff4471befdae812f77ee0d40914d2e
andy <andy@andydote.co.uk>
2014-05-28 19:26:45 +0100

    spark: stub sparkViewRenderer

---
+++ Subvert.Spark/SparkViewRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using Subvert.ViewRendering;
+
+namespace Subvert.Spark
+{
+	public class SparkViewRenderer : IViewRenderer
+	{
+		public bool CanHandle(IRequest request)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public HttpResponseMessage Render(object viewModel)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
