dff25597a1f448c7155b4c012f034d5e74805254
andy <andy@andydote.co.uk>
2014-05-28 19:38:17 +0100

    subvertSparkView: add base class and implementation

---
+++ Subvert.Spark/ISettableModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace Subvert.Spark
+{
+	internal interface ISettableModel
+	{
+		void SetModel(Object model);
+		void RenderView(TextWriter writer);
+	}
+}
---
+++ Subvert.Spark/SubvertSparkView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Spark;
+
+namespace Subvert.Spark
+{
+	public abstract class SubvertSparkView : SparkViewBase
+	{
+	}
+
+	public class SubvertSparkView<TViewModel> : SubvertSparkView, ISettableModel where TViewModel : class
+	{
+		private readonly Guid _id;
+
+		public override Guid GeneratedViewId { get { return _id; } }
+		public TViewModel Model { get; private set; }
+
+		public SubvertSparkView()
+		{
+			_id = Guid.NewGuid();
+		}
+
+		public override void Render()
+		{
+			RenderView(Output);
+		}
+
+		public void SetModel(object model)
+		{
+			Model = (TViewModel)model;
+		}
+	}
+}
