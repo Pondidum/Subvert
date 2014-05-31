c3eddaaf0f60fa48d7785a95342933fd6786c1b9
andy <andy@andydote.co.uk>
2014-05-31 19:59:28 +0100

    configurationAccessor: rename to SubvertConfiguration

---
+++ Subvert/Configuration/SubvertConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using StructureMap;
+using Subvert.ViewRendering;
+
+namespace Subvert.Configuration
+{
+
+	internal class GlobalConfiguration
+	{
+		public static IContainer Container { get; set; }
+	}
+
+	public class SubvertConfiguration
+	{
+		public RendererConfiguration Renderers { get { return _renderers;  } }
+
+		private readonly RendererConfiguration _renderers;
+
+		public SubvertConfiguration()
+		{
+
+			var type = GetType();
+			if (type == typeof(SubvertConfiguration))
+			{
+				//_applicationAssembly = TypePool.FindTheCallingAssembly();
+
+			}
+			else
+			{
+				GlobalConfiguration.Container.GetInstance<HostAssembly>().SetType(type);
+				//_applicationAssembly = type.Assembly;
+			}
+
+			_renderers = GlobalConfiguration.Container.GetInstance<RendererConfiguration>();
+		}
+	}
+}
