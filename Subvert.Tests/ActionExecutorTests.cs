1349b1ab5964da4c5135ed18550f903adaf019f2
andy <andy@andydote.co.uk>
2014-06-07 10:14:50 +0100

    actionExecutor: stub out some tests

---
+++ Subvert.Tests/ActionExecutorTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Subvert.Tests.FrontControllerTests
+{
+	public class ActionExecutorTests
+	{
+		[Fact]
+		public void When_the_action_has_no_input_model()
+		{
+			
+		}
+
+		[Fact]
+		public void When_the_action_has_an_input_model()
+		{
+			
+		}
+	}
+}
