32f2d398af8a468aeb611f859074ac3a516c68d2
andy <andy@andydote.co.uk>
2014-06-09 23:27:42 +0100

    defaultEndpointStore: implement a store which will find convention endpoints (home, error etc)

---
+++ Subvert/DefaultEndpointStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Subvert.Configuration;
+
+namespace Subvert
+{
+	public class DefaultEndpointStore : IEndpointStore
+	{
+		private readonly IEndpointStore _otherStore;
+		private readonly RouteConfiguration _configuration;
+
+		public DefaultEndpointStore(RouteConfiguration configuration, IEndpointStore otherStore)
+		{
+			_configuration = configuration;
+			_otherStore = otherStore;
+		}
+
+		public Endpoint GetEndpointByName(string name)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+			{
+				if (_configuration.HomeEndpoint != null)
+				{
+					return _otherStore.GetEndpointByType(_configuration.HomeEndpoint);
+				}
+
+				return _otherStore.GetEndpointByName("Home");
+			}
+			
+			return _otherStore.GetEndpointByName(name);
+		}
+
+		public Endpoint GetEndpointByType(Type type)
+		{
+			return _otherStore.GetEndpointByType(type);
+		}
+	}
+}
--- Subvert/Router.cs
+++ Subvert/Router.cs
@@ -4,20 +4,16 @@ namespace Subvert
 {
 	public class Router
 	{
-		private readonly RouteConfiguration _configuration;
 		private readonly IEndpointStore _endpoints;
 
-		public Router(RouteConfiguration configuration, IEndpointStore endpoints)
+		public Router(IEndpointStore endpoints)
 		{
-			_configuration = configuration;
 			_endpoints = endpoints;
 		}
 
 		public IEndpointAction GetAction(RouteData route)
 		{
-			var endpoint = route.Endpoint != null 
-				? _endpoints.GetEndpointByName(route.Endpoint) 
-				: _endpoints.GetEndpointByType(_configuration.HomeEndpoint);
+			var endpoint = _endpoints.GetEndpointByName(route.Endpoint);
 
 			if (endpoint == null)
 			{
